89072ba4bcdcf30eae960f74055c1cf04bb768ed
Vítor <mtns.vitor@gmail.com>
2026-09-18 14:56:29 -0300

    feat: add immersive design system lab

---
+++ src/app/features/design-system/design-system-lab-themes.ts
@@ -0,0 +1,46 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'gr-lab-theme-base',
+  template: '',
+  styleUrl: './design-system-lab-base-a.scss',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DesignSystemLabBaseThemeComponent {}
+
+@Component({
+  selector: 'gr-lab-theme-base-extra',
+  template: '',
+  styleUrl: './design-system-lab-base-b.scss',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DesignSystemLabBaseExtraThemeComponent {}
+
+@Component({
+  selector: 'gr-lab-theme-patterns',
+  template: '',
+  styleUrl: './design-system-lab-patterns-a.scss',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DesignSystemLabPatternsThemeComponent {}
+
+@Component({
+  selector: 'gr-lab-theme-patterns-extra',
+  template: '',
+  styleUrl: './design-system-lab-patterns-b.scss',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DesignSystemLabPatternsExtraThemeComponent {}
+
+@Component({
+  selector: 'gr-lab-theme-responsive',
+  template: '',
+  styleUrl: './design-system-lab-responsive.scss',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DesignSystemLabResponsiveThemeComponent {}
